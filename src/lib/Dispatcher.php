96c463abf9c767be3231a01d49b4a01a40651c62
yruefenacht <yannick.ruefenacht@hotmail.com>
2019-09-25 14:24:17 +0200

    Dispatcher bug fix

--- src/lib/Dispatcher.php
+++ src/lib/Dispatcher.php
@@ -9,15 +9,15 @@ class Dispatcher
         $uri = trim($uri, '/');             //Alle / am anfang und am Ende der URI abschneiden
         $uriFragments = explode('/', $uri); //In einzelteile zerlegen
         $controllerName = 'DefaultController';
-        if (!empty($uriFragments[1])) {
-            $controllerName = $uriFragments[1];
+        if (!empty($uriFragments[0])) {
+            $controllerName = $uriFragments[0];
             $controllerName = ucfirst($controllerName); //Erstes Zeichen grossschreiben
             $controllerName .= 'Controller';            //"Controller" anhängen
         }
         
         $method = 'index';
-        if (!empty($uriFragments[2])) {
-            $method = $uriFragments[2];
+        if (!empty($uriFragments[1])) {
+            $method = $uriFragments[1];
         }
         require_once "src/controller/$controllerName.php";
 
