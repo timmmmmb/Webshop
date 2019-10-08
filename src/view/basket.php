e73c269bdf9ac54759267f0350cb6d25a0957eaf
Tim Frey <frey.tim@oublook.com>
2019-10-08 14:39:26 +0200

    changed the button size

--- src/view/basket.php
+++ src/view/basket.php
@@ -14,20 +14,20 @@
         <ul>
             <?php foreach ($products as $product) : ?>
                 <li><?= $product->amount . " " . $product->color . " " . $product->name . " " . $product->size . " Preis: " . $product->prize . " CHF total: " . $product->total_prize . " CHF" ?>
-                    <form action="/order/removeItem" method="post">
-                        <input type="hidden" name="id" value="<?= $product->order_id ?>" />
-                        <input type="hidden" name="amount" value="<?= $product->amount ?>" />
-                        <button type="submit">remove</button>
-                    </form>
                     <form action="/order/updateAmount" method="post">
                         <input type="hidden" name="id" value="<?= $product->order_id ?>" />
                         <input type="number" name="amount" min="1" value="<?= $product->amount ?>">
-                        <button type="submit">Update Amount</button>
+                        <button class="checkoutButton" type="submit">Update Amount</button>
+                    </form>
+                    <form action="/order/removeItem" method="post">
+                        <input type="hidden" name="id" value="<?= $product->order_id ?>" />
+                        <input type="hidden" name="amount" value="<?= $product->amount ?>" />
+                        <button class="checkoutButton" type="submit">remove</button>
                     </form>
                 </li>
             <?php endforeach; ?>
             <form action="/product/payForm" method="post">
-                <button type="submit">Pay</button>
+                <button class="checkoutButton" type="submit">Pay</button>
             </form>
         </ul>
     <?php endif; ?>
