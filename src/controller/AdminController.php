2d61802da4588cb06ad6afe7fe5dff979a40ff86
yruefenacht <yannick.ruefenacht@hotmail.com>
2019-12-23 15:56:28 +0100

    Image upload now works

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -3,6 +3,7 @@
 require_once 'src/model/UserModel.php';
 require_once 'src/model/ProductModel.php';
 require_once 'src/model/OrderModel.php';
+require_once 'src/lib/InputValidator.php';
 
 /**
  * URL name: /admin
@@ -71,12 +72,74 @@ class AdminController
     public function createProduct()
     {
         $this->checkForAdminRights("/");
-        $target_dir = "/images/upload/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        //it doesnt upload the files?
-        var_dump($_FILES);
-        die();
-        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+        if (
+            isset($_POST['name_de']) && 
+            isset($_POST['name_en']) &&
+            isset($_POST['description_de']) &&
+            isset($_POST['description_en']) &&
+            isset($_POST['gender']) &&
+            isset($_POST['price']) &&
+            isset($_FILES['image']))
+        {
+            $iv = new InputValidator();
+            $productModel = new ProductModel();
+            try
+            {
+                $name_de = $iv->validateString($_POST['name_de']);
+                $name_en = $iv->validateString($_POST['name_en']);
+                $description_de = $iv->validateString($_POST['description_de']);
+                $description_en = $iv->validateString($_POST['description_en']);
+                $price = $iv->validateByRegex($_POST['price'], "@^\d+\.\d+$@");
+                $gender = $_POST['gender'];
+                $image = $_FILES['image'];
+            }
+            catch (\Exception $e)
+            {
+                echo $e->getMessage();
+                die();
+            }
+            
+            $target_dir = "src/view/images/";
+            $target_file = $target_dir . basename($image["name"]);
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            //Check if file already exists
+            if (file_exists($target_file)) 
+            {
+                echo "Sorry, file already exists.";
+                die();
+            }
+            //Allow certain file formats
+            if ($imageFileType != "png") 
+            {
+                echo "Sorry, only PNG files are allowed.";
+                die();
+            }
+            //Allow specific size
+            $imageSize = getimagesize($image["tmp_name"]);
+            if ($imageSize[0] != $imageSize[1])
+            {
+                echo "Sorry, image must have same width as height";
+                die();
+            }
+            //Upload
+            if (move_uploaded_file($image["tmp_name"], $target_file)) 
+            {
+                $imageName = basename($image["name"]);
+                echo "The file ". $imageName . " has been uploaded.";
+                $productModel->insertProduct(
+                    $name_de, 
+                    $name_en, 
+                    $description_de, 
+                    $description_en, 
+                    $gender, 
+                    $imageName, 
+                    $price);
+            } 
+            else 
+            {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
     }
     
     /**
@@ -85,9 +148,18 @@ class AdminController
      */
     private function checkForAdminRights($redirect)
     {
-        if (isset($_SESSION['user_type']) && $_SESSION["user_type"] != "Admin") {
+        if (!isset($_SESSION['user_type'])) 
+        {
             header("Location: ".$redirect);
             die();
+        }
+        else
+        {
+            if ($_SESSION["user_type"] != "Admin") 
+            {
+                header("Location: ".$redirect);
+                die();
+            }
         }
     }
 }
